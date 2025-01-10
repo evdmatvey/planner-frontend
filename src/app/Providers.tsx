9a23ee87455cc18da7f8ca45b94e0adcbf99a4c6
evdmatvey <evd.matvey@mail.ru>
2025-01-10 22:46:35 +0700

    Add toast conainer

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,6 +1,20 @@
+import { Toaster } from 'react-hot-toast';
 import { RouterProvider } from 'react-router';
 import { router } from './router/router';
 
 export const Providers = () => {
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <>
+      <Toaster
+        toastOptions={{
+          position: 'top-right',
+          style: {
+            color: 'var(--text)',
+            background: 'var(--toast-background)',
+          },
+        }}
+      />
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  );
 };
