cf525df4a77c378f89e1642388c0e51c10c5f7ed
evdmatvey <evd.matvey@mail.ru>
2025-01-12 17:25:34 +0700

    Add isSuccess to use get profile return

--- src/entities/user/model/useGetProfile.ts
+++ src/entities/user/model/useGetProfile.ts
@@ -2,11 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 import { userQueries } from '../api/user.queries';
 
 export const useGetProfile = () => {
-  const { data, isLoading, isError, error } = useQuery(userQueries.profile());
+  const { data, error, isLoading, isError, isSuccess } = useQuery(
+    userQueries.profile(),
+  );
 
   return {
     data,
     isLoading,
+    isSuccess,
     isError,
     error,
   };
