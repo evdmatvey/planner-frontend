14551e1e0092f24f6c5eaa200a10dfdbf7d7520e
evdmatvey <evd.matvey@mail.ru>
2025-01-15 15:41:47 +0700

    Add task card component

--- src/entities/task/index.ts
+++ src/entities/task/index.ts
@@ -4,3 +4,4 @@ export * from './api/dto/task.response';
 export * from './model/task.types';
 export * from './api/task.queries';
 export * from './api/task.service';
+export * from './ui/TaskCard';
