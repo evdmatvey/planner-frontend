001f926fba7671749d0159f2bc4cea71b0ec9d21
evdmatvey <evd.matvey@mail.ru>
2025-01-15 15:40:56 +0700

    Add tasks state to store

--- src/pages/Tasks/model/store.ts
+++ src/pages/Tasks/model/store.ts
@@ -1,13 +1,20 @@
 import { create } from 'zustand';
+import { Task } from '@/entities/task';
 
 export type TasksView = 'board' | 'list';
 
 interface TasksStore {
   tasksView: TasksView;
+  tasks: Task[];
   setTasksView: (tasksView: TasksView) => void;
+  setTasks: (tasks: Task[]) => void;
+  addTask: (task: Task) => void;
 }
 
 export const useTasksStore = create<TasksStore>()((set) => ({
   tasksView: 'board',
+  tasks: [],
   setTasksView: (tasksView: TasksView) => set({ tasksView }),
+  setTasks: (tasks: Task[]) => set({ tasks }),
+  addTask: (task: Task) => set((store) => ({ tasks: [...store.tasks, task] })),
 }));
