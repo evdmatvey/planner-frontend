e2d41a8867e4d1baedd993f057529ce079be85d7
evdmatvey <evd.matvey@mail.ru>
2025-01-16 17:14:47 +0700

    Add date picker component

--- src/entities/task/api/dto/create-task.dto.ts
+++ src/entities/task/api/dto/create-task.dto.ts
@@ -4,7 +4,7 @@ import { TaskPriority } from '../../model/task.types';
 export interface CreateTaskDto {
   title: string;
   description?: string;
-  createdAt?: number;
+  createdAt?: string;
   executionTime?: number;
   priority?: TaskPriority;
   tags?: Tag[];
