b85764f42b4e69750fedcd11d91a90df64437830
evdmatvey <evd.matvey@mail.ru>
2025-01-20 14:26:55 +0700

    Add dnd for tasks list view

--- src/pages/Tasks/ui/ListRow/index.tsx
+++ src/pages/Tasks/ui/ListRow/index.tsx
@@ -1,6 +1,8 @@
+import { Draggable, Droppable } from '@hello-pangea/dnd';
 import { type Task } from '@/entities/task';
 import { filterTasksByGroup } from '../../lib/filter-tasks-by-group';
 import { type TaskGroup } from '../../model/task-groups';
+import { ListTask } from '../ListTask';
 import styles from './ListRow.module.css';
 
 interface ListRowProps {
@@ -14,11 +16,30 @@ export const ListRow = ({ group, tasks }: ListRowProps) => {
   return (
     <div className={styles.root}>
       <div className={styles.title}>{group.label}</div>
-      <div className={styles.tasks}>
-        {filteredTasks.map((task) => (
-          <div>{task.title}</div>
-        ))}
-      </div>
+      <Droppable droppableId={group.value}>
+        {(provided) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className={styles.tasks}
+          >
+            {filteredTasks.map((task, index) => (
+              <Draggable key={task.id} draggableId={task.id} index={index}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <ListTask task={task} />
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
     </div>
   );
 };
---
+++ src/pages/Tasks/ui/ListTask/index.tsx
@@ -0,0 +1,77 @@
+import dayjs from 'dayjs';
+import { Controller, useForm } from 'react-hook-form';
+import { SelectOrCreateTagPopup } from '@/entities/tag';
+import { CreateTaskDto, SelectTaskPriority, Task } from '@/entities/task';
+import { DatePicker } from '@/shared/ui/DatePicker';
+import { TransparentInput } from '@/shared/ui/TransparentInput';
+import { DeleteIcon } from '@/shared/ui/icons/DeleteIcon';
+import { MoveIcon } from '@/shared/ui/icons/MoveIcon';
+import styles from './ListTask.module.css';
+
+interface ListTaskProps {
+  task: Task;
+}
+
+export const ListTask = ({ task }: ListTaskProps) => {
+  const { register, control } = useForm<CreateTaskDto>({
+    defaultValues: {
+      title: task.title,
+      description: task.description,
+      createdAt: task.createdAt,
+      tags: task.tags,
+      priority: task.priority,
+    },
+  });
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.move}>
+        <MoveIcon />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.cell}>
+          <TransparentInput {...register('title')} />
+        </div>
+        <div className={styles.cell}>
+          <TransparentInput {...register('description')} />
+        </div>
+        <div className={styles.cell}>
+          <Controller
+            control={control}
+            name="createdAt"
+            render={({ field: { value, onChange } }) => (
+              <DatePicker
+                date={dayjs(value).format('DD.MM.YYYY')}
+                onChange={onChange}
+              />
+            )}
+          />
+        </div>
+        <div className={styles.cell}>
+          <Controller
+            control={control}
+            name="tags"
+            render={({ field: { value, onChange } }) => (
+              <SelectOrCreateTagPopup
+                defaultTags={value ?? []}
+                onChange={onChange}
+              />
+            )}
+          />
+        </div>
+        <div className={styles.cell}>
+          <Controller
+            control={control}
+            name="priority"
+            render={({ field: { value, onChange } }) => (
+              <SelectTaskPriority defaultPriority={value} onChange={onChange} />
+            )}
+          />
+        </div>
+      </div>
+      <button className={styles.delete}>
+        <DeleteIcon />
+      </button>
+    </div>
+  );
+};
