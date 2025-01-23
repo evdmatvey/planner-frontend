bc64189381ac7e7d93fbecec9323fb244da8d66b
evdmatvey <evd.matvey@mail.ru>
2025-01-23 21:58:43 +0700

    Add forward ref to icon button component

--- src/shared/ui/IconButton/index.tsx
+++ src/shared/ui/IconButton/index.tsx
@@ -1,17 +1,19 @@
 import clsx from 'clsx';
-import { type ComponentProps, type ReactNode } from 'react';
+import { type ComponentProps, type ReactNode, forwardRef } from 'react';
 import styles from './IconButton.module.css';
 
 interface IconButtonProps extends ComponentProps<'button'> {
   icon: ReactNode;
 }
 
-export const IconButton = ({ icon, className, ...props }: IconButtonProps) => {
-  const classes = clsx(styles.root, className);
+export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  ({ icon, className, ...props }, ref) => {
+    const classes = clsx(styles.root, className);
 
-  return (
-    <button className={classes} {...props}>
-      {icon}
-    </button>
-  );
-};
+    return (
+      <button ref={ref} className={classes} {...props}>
+        {icon}
+      </button>
+    );
+  },
+);
