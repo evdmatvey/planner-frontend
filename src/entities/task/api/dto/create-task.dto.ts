f669f930feeadf6198e9e3dc5dfe5c1ce731fa9c
evdmatvey <evd.matvey@mail.ru>
2025-01-24 10:25:32 +0700

    Fix build issues

--- src/entities/task/api/dto/create-task.dto.ts
+++ src/entities/task/api/dto/create-task.dto.ts
@@ -1,11 +1,11 @@
+import { Priority } from '@/entities/priority/model/priority.types';
 import { Tag } from '@/entities/tag/@x/task';
-import { TaskPriority } from '../../model/task.types';
 
 export interface CreateTaskDto {
   title: string;
   description?: string;
   createdAt?: string;
   executionTime?: number;
-  priority?: TaskPriority;
+  priority?: Priority;
   tags?: Tag[];
 }
