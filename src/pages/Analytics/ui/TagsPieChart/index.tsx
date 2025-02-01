4bd08d8d800512d022d8892e3997695b3e3af36c
evdmatvey <evd.matvey@mail.ru>
2025-02-01 22:55:50 +0700

    Add tags pie chart

---
+++ src/pages/Analytics/ui/TagsPieChart/index.tsx
@@ -0,0 +1,59 @@
+import { useQuery } from '@tanstack/react-query';
+import {
+  ArcElement,
+  ChartData,
+  Chart as ChartJS,
+  ChartOptions,
+  Legend,
+  Tooltip,
+} from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import { analyticsQueries } from '@/entities/analytics';
+import { chartColors } from '../../model/chart-colors.data';
+import styles from './TagsChart.module.css';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const options: ChartOptions<'pie'> = {
+  plugins: {
+    legend: {
+      position: 'bottom',
+      align: 'center',
+    },
+    tooltip: {
+      enabled: true,
+    },
+  },
+};
+
+export const TagsPieChart = () => {
+  const { data: tagAnalytics } = useQuery(analyticsQueries.tag());
+
+  console.log(tagAnalytics);
+
+  const data: ChartData<'pie'> = {
+    labels: tagAnalytics?.map((tag) => tag.title),
+    datasets: [
+      {
+        label: 'Кол-во задач',
+        data:
+          tagAnalytics?.map((tag) =>
+            tag.tasks.reduce((acc, data) => acc + data.tasks.all.count, 0),
+          ) || [],
+        backgroundColor: tagAnalytics?.map(
+          (tag) => chartColors[tag.color].backgroundColor,
+        ),
+        borderColor: tagAnalytics?.map(
+          (tag) => chartColors[tag.color].borderColor,
+        ),
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <div className={styles.root}>
+      <Pie data={data} options={options} />
+    </div>
+  );
+};
