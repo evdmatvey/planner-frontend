e7470c53ddd84e7d867ce6de16be0a1c855fbe21
evdmatvey <evd.matvey@mail.ru>
2025-06-17 14:26:21 +0700

    Update home page

--- src/pages/Home/ui/Home/index.tsx
+++ src/pages/Home/ui/Home/index.tsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router';
+import { DashboardDrawer } from '@/widgets/dashboard-drawer';
 import { DashboardProfile } from '@/widgets/dashboard-profile';
+import { brakepointsConfig } from '@/shared/config/brakepoints';
 import { routesConfig } from '@/shared/config/routes';
+import { getVw } from '@/shared/lib/get-vw';
 import { AccessTokenStorage } from '@/shared/lib/requester';
 import { Logo } from '@/shared/ui/Logo';
 import styles from './Home.module.css';
@@ -9,19 +12,25 @@ import styles from './Home.module.css';
 
 export const Home = () => {
   const isTokenExist = AccessTokenStorage.getToken();
+  const isMobile = getVw() < brakepointsConfig.mobile;
 
   return (
     <div className={styles.root}>
       <header className={styles.header}>
         <Logo />
         {isTokenExist ? (
-          <DashboardProfile />
+          isMobile ? (
+            <DashboardDrawer />
+          ) : (
+            <DashboardProfile />
+          )
         ) : (
           <Link to={routesConfig.LOGIN} className={styles.link}>
             Войти
           </Link>
         )}
       </header>
+      <div className="container">Приложение для планирования Planner</div>
     </div>
   );
 };
